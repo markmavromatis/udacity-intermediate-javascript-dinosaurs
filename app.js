54d8b0932c8c430021b7402aecdac650992c9093
Mark Mavromatis <markmavromatis@Marks-2020-Macbook-Pro-13.local>
2020-09-01 22:50:43 -0500

    Implemented trivia generator

--- app.js
+++ app.js
@@ -13,20 +13,24 @@
     }
 
     // Create Dino Objects
-    const dinos = fetch("./dino.json")
+    const dinosData = {};
+    fetch("./dino.json")
             .then((response) => response.json())
             .catch((error) => console.error("Error reading dinosaurs file!"))
             .then((data) => {
-                const dinosData = data.Dinos;
-                return data.Dinos;
+                data.Dinos.forEach((aDino) => {
+                    dinosData[aDino.species.toLowerCase()] = aDino;
+                })
             })
+
 
     // Use IIFE to get human data from form
     const getHumanData = (function() {
         return function() {
             const name = document.getElementById("name").value;
             const heightFeet = document.getElementById("feet").value;
-            const heightInches = document.getElementById("inches").value;
+            let heightInches = document.getElementById("inches").value;
+            if (!heightInches) {heightInches = 0};
             const weight = document.getElementById("weight").value;
             return {name: name, heightInches: heightInches + heightFeet * 12, weight: weight}
         }
@@ -50,12 +54,39 @@
 
     // Remove form from screen
 
+function getMissingFieldFromForm() {
+    const nameField = document.getElementById("name").value;
+
+    if (!nameField) {
+        return "Name";
+    }
+
+    const heightField = document.getElementById("feet").value;
+    if (!heightField) {
+        return "Height";
+    }
+
+    const weightField = document.getElementById("weight").value;
+    if (!weightField) {
+        return "Weight";
+    }
+    return null;
+}
 
 // On button click, prepare and display infographic
 document.getElementById("btn").onclick = () => {
 
-    // console.log(getHumanData())
-    document.getElementById("CloseButton").className="visible";
+    const errorMessageNode = document.getElementById("formMessage");
+    errorMessageNode.innerHTML = ``;
+    const missingField = getMissingFieldFromForm();
+    if (missingField) {
+        errorMessageNode.innerHTML = `${missingField} is required!`;
+        return false;
+    }
+
+    const formData = getHumanData();
+
+    document.getElementById("tryAgainBtn").className="inline-block";
 
     // Hide form
     const gridNode = document.getElementById("grid");
@@ -64,7 +95,7 @@ document.getElementById("btn").onclick = () => {
 
         const descriptionNode = document.createElement("h3");
         descriptionNode.innerHTML = tileType;
-        const trivia = getTriviaForTile(tileType);
+        const trivia = getTriviaForTile(tileType, formData);
         const triviaNode = document.createElement("p");
         triviaNode.innerHTML = trivia;
         const newDiv = document.createElement("div");
@@ -80,7 +111,7 @@ document.getElementById("btn").onclick = () => {
 }
 
 
-document.getElementById("CloseButton").onclick = () => {
+document.getElementById("tryAgainBtn").onclick = () => {
 
     // Remove dinosaur tiles
     const gridNode = document.getElementById("grid");
@@ -89,7 +120,7 @@ document.getElementById("CloseButton").onclick = () => {
     }
 
     // Hide this button
-    document.getElementById("CloseButton").className="invisible";
+    document.getElementById("tryAgainBtn").className="invisible";
 
     // Show form
     document.getElementById("dino-compare").className = "visible";
@@ -97,15 +128,79 @@ document.getElementById("CloseButton").onclick = () => {
 
 
 //TODO: Add trivia for dinosaurs
-function getTriviaForTile(species) {
+function getTriviaForTile(species, formData) {
     if (species == "pigeon") {
         return "All birds are dinosaurs."
     } else if (species == "human") {
-        return "Human Name"
+        return formData.name
     } else {
-        return "Everybody walk a dinosaur!"
+        return getTriviaForDinosaur(dinosData[species], formData);
     }
 }
 
 //TODO: Generate 3 methods to compare dinosaur data with human data
+const getTriviaForDinosaur = function(speciesData, humanData) {
+
+
+    function getTimePeriod() {
+        return `Lived during the ${speciesData.when} period`;
+    }
+
+    function getLocation() {
+        return `Lived in the following regions: ${speciesData.where}`;
+    }
+
+    function getFact() {
+        return speciesData.fact;
+    }
+
+    function compareWeight() {
+        const humanName = humanData.name;
+        const dinosaurWeight = speciesData.weight
+        const humanWeight = humanData.weight;
+        if (dinosaurWeight > humanWeight) {
+            return `This dinosaur was heavier than ${humanName}: ${dinosaurWeight}lbs vs ${humanWeight}lbs`;
+        } else if (dinosaurWeight == humanWeight) {
+            return `This dinosaur weighs the same as ${humanName}! ${dinosaurWeight}lbs`;
+        } else {
+            return `This dinosaur weighs LESS than ${humanName}! ${dinosaurWeight}lbs vs ${humanWeight}lbs`;
+        }
+    }
+
+    function compareHeight() {
+        const humanName = humanData.name;
+        const dinosaurHeight = speciesData.height;
+        const humanHeight = humanData.height;
+        if (dinosaurHeight > humanHeight) {
+            return `This dinosaur was taller than ${humanName}: ${dinosaurHeight} inches vs ${humanHeight} inches`;
+        } else if (dinosaurHeight == humanHeight) {
+            return `This dinosaur was the same height as ${humanName}! ${dinosaurHeight} inches`;
+        } else {
+            return `This dinosaur was shorter than ${humanName}! ${dinosaurHeight} inches vs ${humanHeight} inches`;
+        }
+    }
+
+    function compareDiet() {
+        const humanName = humanData.name;
+        const dinosaurDiet = speciesData.diet
+        const humanDiet = humanData.diet;
+        if (dinosaurDiet == humanDiet) {
+            return `Shared the same diet as ${humanName}`;
+        } else {
+            return `Enjoyed a different diet than ${humanName}! ${dinosaurDiet} vs ${humanDiet}`;
+        }
+    }
+
+    // Generate a random fact
+    const factOption = Math.floor(Math.random() * 6);
+    switch (factOption) {
+        case 0: return getTimePeriod(); break;
+        case 1: return getLocation(); break;
+        case 2: return getFact(); break;
+        case 3: return compareWeight(); break;
+        case 4: return compareHeight(); break;
+        case 5: return compareDiet(); break;
+    }
+}
+
 //TODO: Write logic to render dinosaur trivia
